ce7fbb15286dfc0420fa59da8a162afe85368aac
Rodrigo Candi <rodrigocandi@MacBook-Pro.local>
2025-04-18 10:11:39 +1000

    V1.3.2

--- src/App.js
+++ src/App.js
@@ -179,8 +179,7 @@ export default function BatteryApp() {
                 {installAddons[step - 1].name}
                 <div className="relative group cursor-pointer">
                   <Info size={16} className="text-gray-400" />
-                  <div className="absolute bottom-full mb-2 left-1/2 -translate-x-1/2 whitespace-nowrap bg-zinc-700 text-xs text-white px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 z-50">
-                    {installTooltips[step - 1]}
+                  <div className="absolute bottom-full mb-2 left-1/2 -translate-x-1/2 bg-zinc-700 text-xs text-white px-2 py-2 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 z-50 w-52 text-center leading-tight">                    {installTooltips[step - 1]}
                   </div>
                 </div>
               </span>
